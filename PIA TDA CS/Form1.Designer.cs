046b98fad017ec1ece74dca75a5658df1ed122eb
Vicente Garza Reyna <vgarzareyna@gmail.com>
2020-05-16 12:51:39 -0500

    Agregar archivos de proyecto.

---
+++ PIA TDA CS/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PIA_TDA_CS {
+	partial class Form1 {
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent() {
+			this.label1 = new System.Windows.Forms.Label();
+			this.primaryTextInput = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(19, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(52, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Programa";
+			// 
+			// primaryTextInput
+			// 
+			this.primaryTextInput.AutoCompleteCustomSource.AddRange(new string[] {
+            "programa",
+            "iniciar",
+            "terminar.",
+            "leer",
+            "imprimir"});
+			this.primaryTextInput.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+			this.primaryTextInput.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+			this.primaryTextInput.Location = new System.Drawing.Point(3, 3);
+			this.primaryTextInput.Multiline = true;
+			this.primaryTextInput.Name = "primaryTextInput";
+			this.primaryTextInput.Size = new System.Drawing.Size(199, 281);
+			this.primaryTextInput.TabIndex = 2;
+			this.primaryTextInput.Text = "programa ejemplo;\r\niniciar\r\nleer a;\r\nleer b;\r\nc := a + b;\r\nimprimir c;\r\nterminar." +
+    "";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(3, 290);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(199, 42);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Verificar Programa";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Controls.Add(this.primaryTextInput, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(22, 40);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85.97015F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.02985F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(410, 335);
+			this.tableLayoutPanel1.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(444, 412);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox primaryTextInput;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+	}
+}
+
---
+++ PIA TDA CS/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace PIA_TDA_CS {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+            
+            
+        }
+
+		private void button1_Click(object sender, EventArgs e) {
+			var ids = new List<string>();
+			Regex regex;
+			var line = primaryTextInput.Lines[0];
+			regex = new Regex("^programa [a-z][a-z0-9]*;$");
+			Console.WriteLine(regex.IsMatch(line));
+
+			regex = new Regex("^iniciar$");
+			line = primaryTextInput.Lines[1];
+			Console.WriteLine(regex.IsMatch(line));
+
+			Regex rLeer = new Regex("^leer [a-z][a-z0-9]*;$");
+			Regex rImprimir = new Regex("^imprimir [a-z][a-z0-9]*;$");
+			Regex rAsignar = new Regex("^[a-z][a-z0-9]*\\s?:=[a-z][a-z0-9]*$");
+			var variables = new List<string>();
+			for (int i = 2; i < primaryTextInput.Lines.Length-1; i++) {
+				bool valid = false;
+				string s;
+				line = primaryTextInput.Lines[i];
+				if (rLeer.IsMatch(line)) {
+					s = line.Substring(line.IndexOf(' '), line.Length-line.IndexOf(';'));
+					ids.Add(s);
+					valid = true;
+				}
+				valid |= rImprimir.IsMatch(line);
+				Console.WriteLine(valid);
+			}
+			regex = new Regex("^terminar.$");
+			line = primaryTextInput.Lines[primaryTextInput.Lines.Length - 1];
+			Console.WriteLine(regex.IsMatch(line));
+			Console.WriteLine("====================");
+
+		}
+	}
+}
